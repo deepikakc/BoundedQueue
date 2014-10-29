86cfb5ef87b298a0243775a94fec777e3e279eed
David Zhu <dyzhu12@gmail.com>
2014-10-29 18:03:51 -0400

    Clean up

--- run.sh
+++ run.sh
@@ -11,8 +11,39 @@ Using only primitive types, implement a bounded queue to store integers. The dat
  * dequeue: function should return an integer if one is currently stored in the queue. The function should properly handle the case where the queue is empty.
 
 
-Running Tests
+Running Local Tests
 ------
+In the application root directory, type
+
+```bash
+./run.sh
+```
+
+or compile in the /src directory with
+
+```bash
+javac BoundedQueue.java QueueEmptyException.java QueueFullException.java
+```
+
+```bash
+javac -cp .:../junit-4.11.jar:../hamcrest-core-1.3.jar BoundedQueueTest.java
+```
+
+```bash
+java -cp .:../junit-4.11.jar:../hamcrest-core-1.3.jar org.junit.runner.JUnitCore BoundedQueueTest.java
+```
+
+Running Custom Tests
+------
+All *.java files are located in /src
+
+Source Files
+* BoundedQueue.java - Class definition for the BoundedQueue data structure 
+* QueueEmptyException.java - Used for catching dequeue
+* QueueFullException.java - Used for catching enqueue
+
+Unit Test
+* BoundedQueueTest.java - JUnit Tests
 
 
-
+Add custom JUnit tests to BoundedQueueTest.java and re-run compilitation from the above section.
